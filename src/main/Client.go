76d62b97d126288c08712efe9317d9dc9a706f9d
[wangbintao1992] <[541437863@qq.com]>
2017-12-25 16:56:36 +0800

    nope

--- src/main/Client.go
+++ src/main/Client.go
@@ -4,14 +4,12 @@ import (
 	"flag"
 	"sender"
 	"time"
-	"net/http"
 )
 /*1. 是否成功
 2. 查询缺失
 3. 续传*/
 func main() {
 	//查询缺失
-	http.HandleFunc("/qab",queryServer)
 	inputPath := flag.String("source", "D://go2.pdf", "source")
 	blockSize := flag.Int("bsize", 30000, "block size")
 
@@ -20,7 +18,4 @@ func main() {
 	s.Send()
 
 	time.Sleep(time.Second * 5)
-}
-func queryServer(writer http.ResponseWriter, request *http.Request) {
-
 }
